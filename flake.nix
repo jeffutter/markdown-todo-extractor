015ced0ea6f00d96e7555d1714704fae37007880
Jeffery Utter <jeff@jeffutter.com>
2026-01-17 08:17:31 -0600

    Bump flake and cargo dependencies

--- flake.nix
+++ flake.nix
@@ -2,12 +2,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
     crane.url = "github:ipetkov/crane";
   };
 
